cefc0b10d81eaf925682a3f38ca8ad0d27ba3cfa
Kevin Pinkerton <kevinhpinkerton@gmail.com>
2021-02-01 18:00:59 -0500

    Finished game.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # rock-paper-scissors-exercise
 
+Enter your username in the .env file.
 Run this python file in the command line using "python game.py".
--- game.py
+++ game.py
@@ -1,16 +1,28 @@
 import random
+import os
+from dotenv import load_dotenv
 
-print("Welcome!")
+load_dotenv()
+user = os.getenv("USER_NAME", default="Player One")
+
+print("-------------------")
+
+print("Welcome " + user + "!")
+
+print("-------------------")
 
 user_input = ""
 while(user_input not in ["rock", "paper", "scissors"]):
-    print("Type \"Rock\", \"Paper\", or \"Scissors\"")
+    print("Type \"Rock\", \"Paper\", or \"Scissors\": ", end='')
     user_input = input().lower()
+print("You chose \"" + user_input.capitalize() + "\"")
+
+print("-------------------")
 
 cpu_input = random.choice(["rock", "paper", "scissors"])
-
-print("You chose \"" + user_input.capitalize() + "\"")
 print("Your opponent chose \"" + cpu_input.capitalize() + "\"")
+
+print("-------------------")
 
 inputs = (user_input, cpu_input)
 wins = [("rock", "scissors"), ("scissors", "paper"), ("paper", "rock")]
@@ -23,4 +35,7 @@ elif inputs in ties:
 else:
     print("You lose.")
 
-print("Good game!")
+print("-------------------")
+
+print("Good game! Thanks for playing.")
+ 

e08bd4088d6eff2ff9fc1e5c6a0a84c7c02621fa
Kevin Pinkerton <kevinhpinkerton@gmail.com>
2021-02-03 17:42:17 -0500

    Refactored code and added comments.

--- game.py
+++ game.py
@@ -2,32 +2,36 @@ import random
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-user = os.getenv("USER_NAME", default="Player One")
-
 print("-------------------")
 
+load_dotenv()
+user = os.getenv("USER_NAME", default="Player One") #gets username from .env file.
 print("Welcome " + user + "!")
 
 print("-------------------")
 
+rps = ["rock", "paper", "scissors"]
+
 user_input = ""
-while(user_input not in ["rock", "paper", "scissors"]):
+#loops until user input is correct. 
+while(user_input not in rps): 
     print("Type \"Rock\", \"Paper\", or \"Scissors\": ", end='')
-    user_input = input().lower()
+    user_input = input().lower() #formats and stores user input.
 print("You chose \"" + user_input.capitalize() + "\".")
 
 print("-------------------")
 
-cpu_input = random.choice(["rock", "paper", "scissors"])
+cpu_input = random.choice(rps) #stores random selection.
 print("Your opponent chose \"" + cpu_input.capitalize() + "\".")
 
 print("-------------------")
 
-inputs = (user_input, cpu_input)
+inputs = (user_input, cpu_input) #groups both selections.
+#possible outcomes:
 wins = [("rock", "scissors"), ("scissors", "paper"), ("paper", "rock")]
 ties = [("rock", "rock"), ("scissors", "scissors"), ("paper", "paper")]
 
+#matches selections with possible outcomes.
 if inputs in wins:
     print("You win!")
 elif inputs in ties:
